b009125ab27ac5031c1df48a482446a4aba50d43
Nour Emil <nooremeel1@gmail.com>
2026-06-25 06:32:54 +0300

    disable helmet CSP

--- app.js
+++ app.js
@@ -58,7 +58,9 @@ app.set('views', 'views');
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
 app.use(compression());
 app.use(morgan('combined', { stream: accessLogStream }));
 
